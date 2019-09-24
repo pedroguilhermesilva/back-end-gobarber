4593e7f5e40d497dc7df2b037e24b17edf341de9
Pedro <pedroguilheermesilva@hotmail.com>
2019-09-24 00:55:05 -0300

    send emails cancellations appointments

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '06ee3c6584f9f8',
+    pass: '9ff3982d7a1825',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
