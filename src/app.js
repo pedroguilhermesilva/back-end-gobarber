fd92f814ff4f4b21814b9437701e21fd10e3423d
Pedro <pedroguilheermesilva@hotmail.com>
2019-12-06 00:11:39 -0300

    finished backEnd

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 import express from 'express';
 import path from 'path'; // from node.js
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
+
 import routers from './router';
+import sentryConfig from './config/sentry';
 
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.router();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -21,6 +31,15 @@ class App {
 
   router() {
     this.server.use(routers);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
--- src/lib/Queue.js
+++ src/lib/Queue.js
@@ -2,6 +2,7 @@ import Bee from 'bee-queue';
 import CancellationMail from '../app/jobs/CancellationMail';
 import redisConfig from '../config/redis';
 
+// if need add a new job, we can add here in this vector
 const jobs = [CancellationMail];
 
 class Queue {
@@ -11,6 +12,7 @@ class Queue {
     this.init();
   }
 
+  // inti and conection on redis' database
   init() {
     jobs.forEach(({ key, handle }) => {
       this.queues[key] = {
@@ -22,16 +24,23 @@ class Queue {
     });
   }
 
+  // add jobs on list
   add(queue, job) {
     return this.queues[queue].bee.createJob(job).save();
   }
+  // watch each process of each jobs on list
 
   processQueue() {
     jobs.forEach(job => {
       const { bee, handle } = this.queues[job.key];
 
-      bee.process(handle);
+      bee.on('failed', this.handleFailure).process(handle);
     });
+  }
+
+  // if happen some erro
+  handleFailure(job, err) {
+    console.log(`Queue ${job.queue.name}: FAILED`, err);
   }
 }
 export default new Queue();
